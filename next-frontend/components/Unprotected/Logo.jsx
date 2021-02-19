fb2a4ba929635ceec0ea7e0d28ac3bba53019873
arhammusheer <arhammusheer@gmail.com>
2021-02-19 22:14:52 +0530

    some stuff

--- next-frontend/components/Unprotected/Logo.jsx
+++ next-frontend/components/Unprotected/Logo.jsx
@@ -1,15 +1,18 @@
 import Icon from "@chakra-ui/icon";
 import { Center, Text } from "@chakra-ui/layout";
+import Link from "next/link";
 import { HiLibrary } from "react-icons/hi";
 import ThemeSwitcher from "../ThemeSwitcher";
 export default function Logo() {
   return (
     <Center>
-      <div>
-        <Text fontSize="4xl" mb={0} p={0}>
-          <Icon as={HiLibrary} />
-          Collgr
-        </Text>
+      <div style={{ cursor: "pointer" }}>
+        <Link href="/">
+          <Text fontSize="4xl" mb={0} p={0}>
+            <Icon as={HiLibrary} />
+            Collgr
+          </Text>
+        </Link>
       </div>
     </Center>
   );
--- next-frontend/pages/SAT/index.js
+++ next-frontend/pages/SAT/index.js
@@ -1,5 +1,34 @@
+import { Box, SimpleGrid, Text } from "@chakra-ui/layout";
 import Navbar from "../../components/Protected/Navbar";
+import NoSSR from "react-no-ssr";
 
 export default function SAT() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <SimpleGrid mx={6} mt={10} columns={2} spacing={10}>
+        <Box></Box>
+        <Box height={"80vh"}>
+          <NoSSR>
+            {DisplayPDF(
+              "http://www.orimi.com/pdf-test.pdf"
+            )}
+          </NoSSR>
+        </Box>
+      </SimpleGrid>
+    </>
+  );
+}
+
+function DisplayPDF(link = undefined) {
+  if (link) {
+    return <embed width="100%" height="100%" src={link} />;
+  }
+  return (
+    <Box boxShadow="lg" borderRadius="xl" p={5}>
+      <Text fontSize="2xl" color="gray.300">
+        Opened files display here
+      </Text>
+    </Box>
+  );
 }
