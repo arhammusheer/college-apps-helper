a9b688e236366a86b264f62ae95e451c2dd420d3
arhammusheer <arhammusheer@gmail.com>
2021-02-19 17:38:46 +0530

    First

---
+++ next-frontend/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Collgr</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+    </div>
+  )
+}
