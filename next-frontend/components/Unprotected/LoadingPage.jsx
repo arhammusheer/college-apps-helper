66b29c6a6978e124c808a3d9f7effe3d28006991
arhammusheer <arhammusheer@gmail.com>
2021-02-21 05:31:29 +0530

    edited loader

--- next-frontend/components/Unprotected/LoadingPage.jsx
+++ next-frontend/components/Unprotected/LoadingPage.jsx
@@ -1,8 +1,27 @@
 import { Center } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
-
+import loader from "../../styles/loader.module.css";
 export default function LoadingPage() {
-    return(<>
-    <Center height="100vh"><Spinner/> Loading...</Center>
-    </>)
+  return (
+    <>
+      <Center height="100vh" className={loader.contain}>
+        <div className={loader.loader}>
+          <svg viewBox="0 0 80 80">
+            <circle id="test" cx="40" cy="40" r="32"></circle>
+          </svg>
+        </div>
+
+        <div className={loader.loader} style={{ transform: "rotate(90deg)" }}>
+          <svg viewBox="0 0 80 80">
+            <circle id="test" cx="40" cy="40" r="32"></circle>
+          </svg>
+        </div>
+        <div className={loader.loader} style={{ transform: "rotate(180deg)" }}>
+          <svg viewBox="0 0 80 80">
+            <circle id="test" cx="40" cy="40" r="32"></circle>
+          </svg>
+        </div>
+      </Center>
+    </>
+  );
 }
---
+++ next-frontend/pages/loading.js
@@ -0,0 +1,5 @@
+import LoadingPage from "../components/Unprotected/LoadingPage";
+
+export default function Loading() {
+  return <LoadingPage />;
+}
